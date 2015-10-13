92b3562c5b33d9954c3b4bb8acb72089017f7605
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-13 11:37:52 -0600

    Added header 2 for inspirational quote of the week

--- index.php
+++ index.php
@@ -1,50 +1,52 @@
 <!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8"/>
-			<title>About Kimberly</title>
-		</head>
-		<body>
-			<h1>About Kimberly</h1>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>About Kimberly</title>
+	</head>
+	<body>
+		<h1>About Kimberly</h1>
 
-			<p>Kimberly Ann Keller was born on December 21st, 1989 at St. Joseph's hospital in Albuquerque, NM.
-				Her favorite season is summer in New Mexico. She enjoys green chile, coffee, and lazy Sundays. She
-				has two cats, Renly and Nymeria, named after characters from the George R.R. Martin series, A Song of Ice and Fire.
-				In her spare time she picks up heavy things and puts them down or she plays Magic The Gathering.
-			</p>
+		<p>Kimberly Ann Keller was born on December 21st, 1989 at St. Joseph's hospital in Albuquerque, NM.
+			Her favorite season is summer in New Mexico. She enjoys green chile, coffee, and lazy Sundays. She
+			has two cats, Renly and Nymeria, named after characters from the George R.R. Martin series, A Song of Ice and Fire.
+			In her spare time she picks up heavy things and puts them down or she plays Magic The Gathering.
+		</p>
 
-			<table>
-				<tr>
-					<th>Hobbies/Interests</th>
-					<th>Favorite TV Shows</th>
-				</tr>
-				<tr>
-					<td>Gardening</td>
-					<td>Star Trek</td>
-				</tr>
-				<tr>
-					<td>Weightlifting</td>
-					<td>Battlestar Galactica</td>
-				</tr>
-				<tr>
-					<td>Health &amp; Wellness</td>
-					<td>30 Rock</td>
-				</tr>
-				<tr>
-					<td>Magic The Gathering</td>
-					<td>Doctor Who</td>
-				</tr>
-				<tr>
-					<td>Playing with my cats</td>
-					<td>Too many to list</td>
-				</tr>
-			</table>
+		<table>
+			<tr>
+				<th>Hobbies/Interests</th>
+				<th>Favorite TV Shows</th>
+			</tr>
+			<tr>
+				<td>Gardening</td>
+				<td>Star Trek</td>
+			</tr>
+			<tr>
+				<td>Weightlifting</td>
+				<td>Battlestar Galactica</td>
+			</tr>
+			<tr>
+				<td>Health &amp; Wellness</td>
+				<td>30 Rock</td>
+			</tr>
+			<tr>
+				<td>Magic The Gathering</td>
+				<td>Doctor Who</td>
+			</tr>
+			<tr>
+				<td>Playing with my cats</td>
+				<td>Too many to list</td>
+			</tr>
+		</table>
 
-			<p>
-				<a href="https://instagram.com/lovewedn/" target="_blank">
-					<strong>Click here to follow Kimberly on Instagram</strong>
-				</a>
-			</p>
+		<p>
+			<a href="https://instagram.com/lovewedn/" target="_blank">
+				<strong>Click here to follow Kimberly on Instagram</strong>
+			</a>
+		</p>
 
-		</body>
+		<h2>Inspirational Quote of the Week</h2>
+
+	</body>
 </html>
