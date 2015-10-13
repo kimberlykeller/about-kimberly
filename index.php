a1d20ecf4d25b3ccdcbae46199ea0dfe9122cf40
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-13 10:53:25 -0600

    added beginning body information and the beginnings of a small table

--- index.php
+++ index.php
@@ -4,4 +4,15 @@
 			<meta charset="UTF-8"/>
 			<title>About Kimberly</title>
 		</head>
+		<body>
+			<h1>About Kimberly</h1>
+			<p>Information about Kimberly</p>
+			<table>
+				<tr>
+					<th>Hobbies</th>
+					<th>Favorite TV Shows</th>
+				</tr>
+			</table>
+
+		</body>
 </html>
