efc594a4b7d4b9d08c3e5443ec707e5f4e546e6c
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-13 11:02:16 -0600

    added beginning body information and the beginnings of a small table

--- index.php
+++ index.php
@@ -12,6 +12,22 @@
 					<th>Hobbies</th>
 					<th>Favorite TV Shows</th>
 				</tr>
+				<tr>
+					<td>Hobby 1</td>
+					<td>TV Show 1</td>
+				</tr>
+				<tr>
+					<td>Hobby 2</td>
+					<td>TV Show 2</td>
+				</tr>
+				<tr>
+					<td>Hobby 3</td>
+					<td>TV Show 3</td>
+				</tr>
+				<tr>
+					<td>Hobby 4</td>
+					<td>TV Show 4</td>
+				</tr>
 			</table>
 
 		</body>
